fc004d99c0952b4473440d9dc7ca3dcef018048a
ayush-h-pawar <ayushhpawar0505@gmail.com>
2026-02-14 15:24:14 +0530

    Add files via upload

---
+++ SelectionSort.java
@@ -0,0 +1,27 @@
+public class SelectionSort {
+
+    static void sort(int[] arr) {
+        int n = arr.length;
+
+        for (int i = 0; i < n - 1; i++) {
+            int minIndex = i;
+
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[minIndex])
+                    minIndex = j;
+            }
+
+            int temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {64, 25, 12, 22, 11};
+        sort(arr);
+
+        for (int num : arr)
+            System.out.print(num + " ");
+    }
+}
