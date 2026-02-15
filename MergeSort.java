5f1b5f822db76b1d1c1deb8a67036e64a681ec85
ayush-h-pawar <ayushhpawar0505@gmail.com>
2026-02-15 13:08:22 +0530

    Add files via upload

---
+++ MergeSort.java
@@ -0,0 +1,44 @@
+public class MergeSort {
+
+    static void mergeSort(int[] arr, int left, int right) {
+        if (left >= right) return;
+
+        int mid = left + (right - left) / 2;
+
+        mergeSort(arr, left, mid);
+        mergeSort(arr, mid + 1, right);
+
+        merge(arr, left, mid, right);
+    }
+
+    static void merge(int[] arr, int left, int mid, int right) {
+        int[] temp = new int[right - left + 1];
+
+        int i = left, j = mid + 1, k = 0;
+
+        while (i <= mid && j <= right) {
+            if (arr[i] <= arr[j])
+                temp[k++] = arr[i++];
+            else
+                temp[k++] = arr[j++];
+        }
+
+        while (i <= mid)
+            temp[k++] = arr[i++];
+
+        while (j <= right)
+            temp[k++] = arr[j++];
+
+        for (int p = 0; p < temp.length; p++)
+            arr[left + p] = temp[p];
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {38, 27, 43, 3, 9, 82, 10};
+        mergeSort(arr, 0, arr.length - 1);
+
+        for (int num : arr)
+            System.out.print(num + " ");
+    }
+}
+
