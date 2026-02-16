2f222ef200e6b0a1484fbdd8208a35e08fd70416
ayush-h-pawar <ayushhpawar0505@gmail.com>
2026-02-16 18:49:14 +0530

    Create LinkedListBasic.java

---
+++ LinkedListBasic.java
@@ -0,0 +1,29 @@
+public class LinkedListBasic {
+
+    static class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    static void printList(Node head) {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " -> ");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
+    public static void main(String[] args) {
+        Node head = new Node(10);
+        head.next = new Node(20);
+        head.next.next = new Node(30);
+
+        printList(head);
+    }
+}
