5478c548a223ef3b956bf458b3e4e03f4152a59d
ayush-h-pawar <ayushhpawar0505@gmail.com>
2026-02-22 18:42:05 +0530

    Create LevelOrderTraversal.java

---
+++ LevelOrderTraversal.java
@@ -0,0 +1,42 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class LevelOrderTraversal {
+
+    static class Node {
+        int data;
+        Node left, right;
+
+        Node(int data) {
+            this.data = data;
+        }
+    }
+
+    static void levelOrder(Node root) {
+        if (root == null) return;
+
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(root);
+
+        while (!queue.isEmpty()) {
+            Node current = queue.poll();
+            System.out.print(current.data + " ");
+
+            if (current.left != null)
+                queue.add(current.left);
+
+            if (current.right != null)
+                queue.add(current.right);
+        }
+    }
+
+    public static void main(String[] args) {
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+
+        levelOrder(root);
+    }
+}
