eba7fa0cb82dea9b7f1f21b194de8bab1a3f5293
ayush-h-pawar <ayushhpawar0505@gmail.com>
2026-02-22 18:41:04 +0530

    Create TreeHeight.java

---
+++ TreeHeight.java
@@ -0,0 +1,29 @@
+public class TreeHeight {
+
+    static class Node {
+        int data;
+        Node left, right;
+
+        Node(int data) {
+            this.data = data;
+        }
+    }
+
+    static int height(Node root) {
+        if (root == null) return 0;
+
+        int leftHeight = height(root.left);
+        int rightHeight = height(root.right);
+
+        return 1 + Math.max(leftHeight, rightHeight);
+    }
+
+    public static void main(String[] args) {
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+
+        System.out.println("Height: " + height(root));
+    }
+}
