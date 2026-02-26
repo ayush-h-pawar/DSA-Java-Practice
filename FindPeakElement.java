467f64f04661c64b77cf0880e905b472e2066aa7
ayush-h-pawar <ayushhpawar0505@gmail.com>
2026-02-26 15:08:09 +0530

    Create FindPeakElement.java

---
+++ FindPeakElement.java
@@ -0,0 +1,23 @@
+public class FindPeakElement {
+
+    static int findPeak(int[] nums) {
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] > nums[mid + 1])
+                right = mid;
+            else
+                left = mid + 1;
+        }
+
+        return left;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1,2,3,1};
+        System.out.println(findPeak(nums));
+    }
+}
