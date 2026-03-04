83b2f27409fad3b0ffe231007f92084305e996cd
ayush-h-pawar <ayushhpawar0505@gmail.com>
2026-03-04 21:28:04 +0530

    Create MinimumWindowSubstring.java

---
+++ MinimumWindowSubstring.java
@@ -0,0 +1,63 @@
+import java.util.HashMap;
+
+public class MinimumWindowSubstring {
+
+    static String minWindow(String s, String t) {
+
+        if (s.length() < t.length()) return "";
+
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        for (char c : t.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+
+        int left = 0;
+        int count = map.size();
+        int minLength = Integer.MAX_VALUE;
+        int start = 0;
+
+        for (int right = 0; right < s.length(); right++) {
+
+            char ch = s.charAt(right);
+
+            if (map.containsKey(ch)) {
+                map.put(ch, map.get(ch) - 1);
+                if (map.get(ch) == 0) {
+                    count--;
+                }
+            }
+
+            while (count == 0) {
+
+                if (right - left + 1 < minLength) {
+                    minLength = right - left + 1;
+                    start = left;
+                }
+
+                char leftChar = s.charAt(left);
+
+                if (map.containsKey(leftChar)) {
+                    map.put(leftChar, map.get(leftChar) + 1);
+
+                    if (map.get(leftChar) > 0) {
+                        count++;
+                    }
+                }
+
+                left++;
+            }
+        }
+
+        return minLength == Integer.MAX_VALUE
+                ? ""
+                : s.substring(start, start + minLength);
+    }
+
+    public static void main(String[] args) {
+        String s = "ADOBECODEBANC";
+        String t = "ABC";
+
+        System.out.println(minWindow(s, t));
+    }
+}
