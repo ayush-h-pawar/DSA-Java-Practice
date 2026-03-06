e3c3ea0add9f35b7173a53b61750c678d7ed8f20
ayush-h-pawar <ayushhpawar0505@gmail.com>
2026-03-06 12:47:41 +0530

    Create LinkedListCycleDetection.java

---
+++ LinkedListCycleDetection.java
@@ -0,0 +1,41 @@
+public class LinkedListCycleDetection {
+
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int val) {
+            this.val = val;
+        }
+    }
+
+    static boolean hasCycle(ListNode head) {
+
+        if (head == null) return false;
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if (slow == fast) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = head; // create cycle
+
+        System.out.println(hasCycle(head)); // true
+    }
+}
