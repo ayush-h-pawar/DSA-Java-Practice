7f0d63370c738451c270353f9cfde73ca88d80d1
ayush-h-pawar <ayushhpawar0505@gmail.com>
2026-03-08 12:43:17 +0530

    Create RemoveNthNodeFromEnd.java

---
+++ RemoveNthNodeFromEnd.java
@@ -0,0 +1,57 @@
+public class RemoveNthNodeFromEnd {
+
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int val) {
+            this.val = val;
+        }
+    }
+
+    static ListNode removeNthFromEnd(ListNode head, int n) {
+
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+
+        ListNode fast = dummy;
+        ListNode slow = dummy;
+
+        // Move fast pointer n steps ahead
+        for (int i = 0; i <= n; i++) {
+            fast = fast.next;
+        }
+
+        // Move both pointers
+        while (fast != null) {
+            fast = fast.next;
+            slow = slow.next;
+        }
+
+        // Delete the node
+        slow.next = slow.next.next;
+
+        return dummy.next;
+    }
+
+    static void printList(ListNode head) {
+        while (head != null) {
+            System.out.print(head.val + " -> ");
+            head = head.next;
+        }
+        System.out.println("null");
+    }
+
+    public static void main(String[] args) {
+
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+
+        head = removeNthFromEnd(head, 2);
+
+        printList(head);
+    }
+          }
