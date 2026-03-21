88297768c0cf85cb727873002b8a2c1a9e0b1a24
ayush-h-pawar <ayushhpawar0505@gmail.com>
2026-03-22 00:02:41 +0530

    Create NextGreaterElementII.java

---
+++ NextGreaterElementII.java
@@ -0,0 +1,39 @@
+import java.util.Stack;
+import java.util.Arrays;
+
+public class NextGreaterElementII {
+
+    static int[] nextGreaterElements(int[] nums) {
+
+        int n = nums.length;
+        int[] result = new int[n];
+        Arrays.fill(result, -1);
+
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < 2 * n; i++) {
+
+            int num = nums[i % n];
+
+            while (!stack.isEmpty() && nums[stack.peek()] < num) {
+                result[stack.pop()] = num;
+            }
+
+            if (i < n) {
+                stack.push(i);
+            }
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+
+        int[] nums = {1,2,1};
+
+        int[] result = nextGreaterElements(nums);
+
+        for (int x : result)
+            System.out.print(x + " ");
+    }
+}
