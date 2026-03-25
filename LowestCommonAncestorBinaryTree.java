b94223c25277ef1deaf8972de67c1d601ad8b39a
ayush-h-pawar <ayushhpawar0505@gmail.com>
2026-03-25 09:25:00 +0530

    Create LowestCommonAncestorBinaryTree.java

---
+++ LowestCommonAncestorBinaryTree.java
@@ -0,0 +1,41 @@
+public class LowestCommonAncestorBinaryTree {
+
+    static class TreeNode {
+        int val;
+        TreeNode left, right;
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+    }
+
+    static TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+
+        if (root == null || root == p || root == q)
+            return root;
+
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+
+        if (left != null && right != null)
+            return root;
+
+        return (left != null) ? left : right;
+    }
+
+    public static void main(String[] args) {
+
+        TreeNode root = new TreeNode(3);
+        root.left = new TreeNode(5);
+        root.right = new TreeNode(1);
+        root.left.left = new TreeNode(6);
+        root.left.right = new TreeNode(2);
+
+        TreeNode p = root.left;   // 5
+        TreeNode q = root.right;  // 1
+
+        TreeNode lca = lowestCommonAncestor(root, p, q);
+
+        System.out.println("LCA: " + lca.val); // 3
+    }
+}
