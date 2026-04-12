fcfb18b0d6e005eb0ea3eb75a52d32f5748d0e30
ayush-h-pawar <ayushhpawar0505@gmail.com>
2026-04-12 13:12:38 +0530

    Create PalindromePartitioningII.java

---
+++ PalindromePartitioningII.java
@@ -0,0 +1,32 @@
+public class PalindromePartitioningII {
+
+    static int minCut(String s) {
+
+        int n = s.length();
+
+        boolean[][] isPal = new boolean[n][n];
+        int[] dp = new int[n];
+
+        for (int i = 0; i < n; i++) {
+
+            dp[i] = i;
+
+            for (int j = 0; j <= i; j++) {
+
+                if (s.charAt(i) == s.charAt(j) &&
+                        (i - j <= 2 || isPal[j + 1][i - 1])) {
+
+                    isPal[j][i] = true;
+
+                    if (j == 0) {
+                        dp[i] = 0;
+                    } else {
+                        dp[i] = Math.min(dp[i], dp[j - 1] + 1);
+                    }
+                }
+            }
+        }
+
+        return dp[n - 1];
+    }
+}
