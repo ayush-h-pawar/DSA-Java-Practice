f62a78e5d0d2a14b73024280991c63f21a5b4643
ayush-h-pawar <ayushhpawar0505@gmail.com>
2026-04-14 16:04:06 +0530

    Create FindAllAnagramsInString.java

---
+++ FindAllAnagramsInString.java
@@ -0,0 +1,46 @@
+import java.util.*;
+
+public class FindAllAnagramsInString {
+
+    static List<Integer> findAnagrams(String s, String p) {
+
+        List<Integer> result = new ArrayList<>();
+
+        if (s.length() < p.length()) return result;
+
+        int[] count = new int[26];
+
+        for (char c : p.toCharArray())
+            count[c - 'a']++;
+
+        int left = 0, right = 0, needed = p.length();
+
+        while (right < s.length()) {
+
+            if (count[s.charAt(right) - 'a'] > 0)
+                needed--;
+
+            count[s.charAt(right) - 'a']--;
+            right++;
+
+            if (needed == 0)
+                result.add(left);
+
+            if (right - left == p.length()) {
+
+                if (count[s.charAt(left) - 'a'] >= 0)
+                    needed++;
+
+                count[s.charAt(left) - 'a']++;
+                left++;
+            }
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(findAnagrams("cbaebabacd", "abc")); // [0,6]
+    }
+}
