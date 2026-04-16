06980a0d897f7fea7a038ab43b65efe89d6cff2f
ayush-h-pawar <ayushhpawar0505@gmail.com>
2026-04-16 13:29:23 +0530

    Create NonOverlappingIntervals.java

---
+++ NonOverlappingIntervals.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class NonOverlappingIntervals {
+
+    static int eraseOverlapIntervals(int[][] intervals) {
+
+        Arrays.sort(intervals, (a, b) -> a[1] - b[1]);
+
+        int count = 0;
+        int end = intervals[0][1];
+
+        for (int i = 1; i < intervals.length; i++) {
+
+            if (intervals[i][0] < end) {
+                count++; // remove this interval
+            } else {
+                end = intervals[i][1];
+            }
+        }
+
+        return count;
+    }
+
+    public static void main(String[] args) {
+
+        int[][] intervals = {{1,2},{2,3},{3,4},{1,3}};
+
+        System.out.println(eraseOverlapIntervals(intervals)); // 1
+    }
+}
