b7fa0d1120eb03694009dc74dbf3d60f384d367c
ayush-h-pawar <ayushhpawar0505@gmail.com>
2026-04-26 19:45:25 +0530

    Create AllPathsFromSourceToTarget.java

---
+++ AllPathsFromSourceToTarget.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+public class AllPathsFromSourceToTarget {
+
+    static List<List<Integer>> allPathsSourceTarget(int[][] graph) {
+
+        List<List<Integer>> result = new ArrayList<>();
+        List<Integer> path = new ArrayList<>();
+
+        dfs(0, graph, path, result);
+
+        return result;
+    }
+
+    static void dfs(int node, int[][] graph,
+                    List<Integer> path,
+                    List<List<Integer>> result) {
+
+        path.add(node);
+
+        if (node == graph.length - 1) {
+            result.add(new ArrayList<>(path));
+        } else {
+
+            for (int nei : graph[node]) {
+                dfs(nei, graph, path, result);
+            }
+        }
+
+        path.remove(path.size() - 1);
+    }
+
+    public static void main(String[] args) {
+
+        int[][] graph = {{1,2},{3},{3},{}};
+
+        System.out.println(allPathsSourceTarget(graph));
+    }
+}
