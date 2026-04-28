01bcd698a693e62cbfe7c8b6e040d303915f4c1c
ayush-h-pawar <ayushhpawar0505@gmail.com>
2026-04-28 09:03:55 +0530

    Create CountOfSmallerNumbersAfterSelf.java

---
+++ CountOfSmallerNumbersAfterSelf.java
@@ -0,0 +1,73 @@
+import java.util.*;
+
+public class CountOfSmallerNumbersAfterSelf {
+
+    static class Pair {
+        int val, idx;
+        Pair(int v, int i) {
+            val = v;
+            idx = i;
+        }
+    }
+
+    static List<Integer> countSmaller(int[] nums) {
+
+        int n = nums.length;
+        Pair[] arr = new Pair[n];
+        int[] count = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = new Pair(nums[i], i);
+        }
+
+        mergeSort(arr, 0, n - 1, count);
+
+        List<Integer> result = new ArrayList<>();
+        for (int c : count) result.add(c);
+
+        return result;
+    }
+
+    static void mergeSort(Pair[] arr, int left, int right, int[] count) {
+
+        if (left >= right) return;
+
+        int mid = (left + right) / 2;
+
+        mergeSort(arr, left, mid, count);
+        mergeSort(arr, mid + 1, right, count);
+
+        merge(arr, left, mid, right, count);
+    }
+
+    static void merge(Pair[] arr, int left, int mid, int right, int[] count) {
+
+        List<Pair> temp = new ArrayList<>();
+        int i = left, j = mid + 1;
+        int rightCount = 0;
+
+        while (i <= mid && j <= right) {
+
+            if (arr[j].val < arr[i].val) {
+                rightCount++;
+                temp.add(arr[j++]);
+            } else {
+                count[arr[i].idx] += rightCount;
+                temp.add(arr[i++]);
+            }
+        }
+
+        while (i <= mid) {
+            count[arr[i].idx] += rightCount;
+            temp.add(arr[i++]);
+        }
+
+        while (j <= right) {
+            temp.add(arr[j++]);
+        }
+
+        for (int k = 0; k < temp.size(); k++) {
+            arr[left + k] = temp.get(k);
+        }
+    }
+                  }
