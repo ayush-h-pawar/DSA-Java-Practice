f8a12d5bbaa994fb79c34ac5ae2f3e2e8876e35c
ayush-h-pawar <ayushhpawar0505@gmail.com>
2026-05-03 13:45:29 +0530

    Create MinimumWindowSubstring1.java

---
+++ MinimumWindowSubstring1.java
@@ -0,0 +1,52 @@
+import java.util.*;
+
+public class MinimumWindowSubstring1 {
+
+    static String minWindow(String s, String t) {
+
+        if (s.length() < t.length()) return "";
+
+        int[] count = new int[128];
+
+        for (char c : t.toCharArray())
+            count[c]++;
+
+        int left = 0, right = 0;
+        int required = t.length();
+
+        int minLen = Integer.MAX_VALUE, start = 0;
+
+        while (right < s.length()) {
+
+            if (count[s.charAt(right)] > 0)
+                required--;
+
+            count[s.charAt(right)]--;
+            right++;
+
+            while (required == 0) {
+
+                if (right - left < minLen) {
+                    minLen = right - left;
+                    start = left;
+                }
+
+                count[s.charAt(left)]++;
+
+                if (count[s.charAt(left)] > 0)
+                    required++;
+
+                left++;
+            }
+        }
+
+        return minLen == Integer.MAX_VALUE
+                ? ""
+                : s.substring(start, start + minLen);
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(minWindow("ADOBECODEBANC", "ABC")); // BANC
+    }
+}
