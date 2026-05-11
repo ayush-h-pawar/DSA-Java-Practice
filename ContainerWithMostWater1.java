431327034ac0b4a4486e9dd1a2a658947b44474c
ayush-h-pawar <ayushhpawar0505@gmail.com>
2026-05-11 10:18:49 +0530

    Create ContainerWithMostWater1.java

---
+++ ContainerWithMostWater1.java
@@ -0,0 +1,35 @@
+public class ContainerWithMostWater1 {
+
+    static int maxArea(int[] height) {
+
+        int left = 0;
+        int right = height.length - 1;
+
+        int maxArea = 0;
+
+        while (left < right) {
+
+            int width = right - left;
+
+            int currArea =
+                    Math.min(height[left], height[right]) * width;
+
+            maxArea = Math.max(maxArea, currArea);
+
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        return maxArea;
+    }
+
+    public static void main(String[] args) {
+
+        int[] height = {1,8,6,2,5,4,8,3,7};
+
+        System.out.println(maxArea(height));
+    }
+}
