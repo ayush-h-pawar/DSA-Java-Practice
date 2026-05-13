c674ace0993bc1c819a19b0d5366f4c260e1aac0
ayush-h-pawar <ayushhpawar0505@gmail.com>
2026-05-13 15:23:36 +0530

    Create FirstDefectiveVersionFinder.java

---
+++ FirstDefectiveVersionFinder.java
@@ -0,0 +1,37 @@
+public class FirstDefectiveVersionFinder {
+
+    // Mock API
+    static boolean isBadVersion(int version) {
+
+        int firstBad = 4;
+
+        return version >= firstBad;
+    }
+
+    static int firstBadVersion(int n) {
+
+        int left = 1;
+        int right = n;
+
+        while (left < right) {
+
+            int mid = left + (right - left) / 2;
+
+            if (isBadVersion(mid)) {
+
+                right = mid;
+
+            } else {
+
+                left = mid + 1;
+            }
+        }
+
+        return left;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(firstBadVersion(10));
+    }
+}
