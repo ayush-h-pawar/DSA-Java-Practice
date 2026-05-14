3cb816496861674225bfdaa61c1c8a8075249eaa
ayush-h-pawar <ayushhpawar0505@gmail.com>
2026-05-14 15:25:09 +0530

    Create ZeroShiftArray.java

---
+++ ZeroShiftArray.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class ZeroShiftArray {
+
+    static void moveZeroes(int[] nums) {
+
+        int insertPos = 0;
+
+        for (int num : nums) {
+
+            if (num != 0) {
+                nums[insertPos++] = num;
+            }
+        }
+
+        while (insertPos < nums.length) {
+            nums[insertPos++] = 0;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        int[] nums = {0,1,0,3,12};
+
+        moveZeroes(nums);
+
+        System.out.println(Arrays.toString(nums));
+    }
+}
