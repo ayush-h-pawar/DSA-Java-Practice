7499ef305ef8da62774850665af310f964dd13a5
ayush-h-pawar <ayushhpawar0505@gmail.com>
2026-05-19 15:04:04 +0530

    Create RottingOrangesSimulator.java

---
+++ RottingOrangesSimulator.java
@@ -0,0 +1,87 @@
+import java.util.*;
+
+public class RottingOrangesSimulator {
+
+    static int orangesRotting(int[][] grid) {
+
+        int rows = grid.length;
+        int cols = grid[0].length;
+
+        Queue<int[]> queue =
+                new LinkedList<>();
+
+        int fresh = 0;
+
+        for (int i = 0; i < rows; i++) {
+
+            for (int j = 0; j < cols; j++) {
+
+                if (grid[i][j] == 2) {
+
+                    queue.offer(new int[]{i, j});
+
+                } else if (grid[i][j] == 1) {
+
+                    fresh++;
+                }
+            }
+        }
+
+        int minutes = 0;
+
+        int[][] dirs = {
+                {1,0},
+                {-1,0},
+                {0,1},
+                {0,-1}
+        };
+
+        while (!queue.isEmpty() &&
+               fresh > 0) {
+
+            int size = queue.size();
+
+            for (int i = 0; i < size; i++) {
+
+                int[] curr = queue.poll();
+
+                for (int[] d : dirs) {
+
+                    int nr = curr[0] + d[0];
+                    int nc = curr[1] + d[1];
+
+                    if (nr >= 0 && nc >= 0 &&
+                        nr < rows && nc < cols &&
+                        grid[nr][nc] == 1) {
+
+                        grid[nr][nc] = 2;
+                        fresh--;
+
+                        queue.offer(
+                                new int[]{nr, nc}
+                        );
+                    }
+                }
+            }
+
+            minutes++;
+        }
+
+        return fresh == 0
+                ? minutes
+                : -1;
+    }
+
+    public static void main(String[] args) {
+
+        int[][] grid = {
+                {2,1,1},
+                {1,1,0},
+                {0,1,1}
+        };
+
+        System.out.println(
+                orangesRotting(grid)
+        );
+    }
+  }
