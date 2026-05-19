306a5141471d5ccbd28a7c6e6b45a277da8fb017
ayush-h-pawar <ayushhpawar0505@gmail.com>
2026-05-19 15:06:01 +0530

    Create IslandPerimeterCalculator.java

---
+++ IslandPerimeterCalculator.java
@@ -0,0 +1,49 @@
+public class IslandPerimeterCalculator {
+
+    static int islandPerimeter(int[][] grid) {
+
+        int rows = grid.length;
+        int cols = grid[0].length;
+
+        int perimeter = 0;
+
+        for (int i = 0; i < rows; i++) {
+
+            for (int j = 0; j < cols; j++) {
+
+                if (grid[i][j] == 1) {
+
+                    perimeter += 4;
+
+                    if (i > 0 &&
+                        grid[i - 1][j] == 1) {
+
+                        perimeter -= 2;
+                    }
+
+                    if (j > 0 &&
+                        grid[i][j - 1] == 1) {
+
+                        perimeter -= 2;
+                    }
+                }
+            }
+        }
+
+        return perimeter;
+    }
+
+    public static void main(String[] args) {
+
+        int[][] grid = {
+                {0,1,0,0},
+                {1,1,1,0},
+                {0,1,0,0},
+                {1,1,0,0}
+        };
+
+        System.out.println(
+                islandPerimeter(grid)
+        );
+    }
+}
