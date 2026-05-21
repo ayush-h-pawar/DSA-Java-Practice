bc3887feb39fb8424801ecd56e2804df07fd5669
ayush-h-pawar <ayushhpawar0505@gmail.com>
2026-05-21 15:36:36 +0530

    Create SparseMatrixMultiplication.java

---
+++ SparseMatrixMultiplication.java
@@ -0,0 +1,34 @@
+public class SparseMatrixMultiplication {
+
+    static int[][] multiply(int[][] mat1,
+                            int[][] mat2) {
+
+        int m = mat1.length;
+        int n = mat2[0].length;
+        int k = mat2.length;
+
+        int[][] result =
+                new int[m][n];
+
+        for (int i = 0; i < m; i++) {
+
+            for (int j = 0; j < k; j++) {
+
+                if (mat1[i][j] != 0) {
+
+                    for (int l = 0; l < n; l++) {
+
+                        if (mat2[j][l] != 0) {
+
+                            result[i][l] +=
+                                    mat1[i][j] *
+                                    mat2[j][l];
+                        }
+                    }
+                }
+            }
+        }
+
+        return result;
+    }
+}
