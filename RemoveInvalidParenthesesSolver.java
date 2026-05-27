d04955b24ab2d25895331d640488353f381e3f0e
ayush-h-pawar <ayushhpawar0505@gmail.com>
2026-05-27 15:23:57 +0530

    Create RemoveInvalidParenthesesSolver.java

---
+++ RemoveInvalidParenthesesSolver.java
@@ -0,0 +1,91 @@
+import java.util.*;
+
+public class RemoveInvalidParenthesesSolver {
+
+    static List<String> removeInvalidParentheses(
+            String s) {
+
+        List<String> result =
+                new ArrayList<>();
+
+        Queue<String> queue =
+                new LinkedList<>();
+
+        Set<String> visited =
+                new HashSet<>();
+
+        queue.offer(s);
+        visited.add(s);
+
+        boolean found = false;
+
+        while (!queue.isEmpty()) {
+
+            String curr = queue.poll();
+
+            if (isValid(curr)) {
+
+                result.add(curr);
+
+                found = true;
+            }
+
+            if (found)
+                continue;
+
+            for (int i = 0;
+                 i < curr.length();
+                 i++) {
+
+                char c = curr.charAt(i);
+
+                if (c != '(' &&
+                    c != ')')
+                    continue;
+
+                String next =
+                        curr.substring(0, i) +
+                        curr.substring(i + 1);
+
+                if (!visited.contains(next)) {
+
+                    visited.add(next);
+
+                    queue.offer(next);
+                }
+            }
+        }
+
+        return result;
+    }
+
+    static boolean isValid(String s) {
+
+        int count = 0;
+
+        for (char c : s.toCharArray()) {
+
+            if (c == '(')
+                count++;
+
+            else if (c == ')') {
+
+                count--;
+
+                if (count < 0)
+                    return false;
+            }
+        }
+
+        return count == 0;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(
+                removeInvalidParentheses(
+                        "()())()"
+                )
+        );
+    }
+}
