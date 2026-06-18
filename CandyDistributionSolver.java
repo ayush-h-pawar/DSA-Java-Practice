eac6f616dcf74d981a4e79ef64d65fa8e4877d57
ayush-h-pawar <ayushhpawar0505@gmail.com>
2026-06-18 09:01:16 +0530

    Implement CandyDistributionSolver class

---
+++ CandyDistributionSolver.java
@@ -0,0 +1,55 @@
+public class CandyDistributionSolver {
+
+    public int candy(int[] ratings) {
+
+        int n = ratings.length;
+
+        if (n == 0) {
+            return 0;
+        }
+
+        int[] candies = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            candies[i] = 1;
+        }
+
+        for (int i = 1; i < n; i++) {
+
+            if (ratings[i] > ratings[i - 1]) {
+                candies[i] = candies[i - 1] + 1;
+            }
+        }
+
+        for (int i = n - 2; i >= 0; i--) {
+
+            if (ratings[i] > ratings[i + 1]) {
+                candies[i] =
+                        Math.max(
+                                candies[i],
+                                candies[i + 1] + 1
+                        );
+            }
+        }
+
+        int totalCandies = 0;
+
+        for (int candy : candies) {
+            totalCandies += candy;
+        }
+
+        return totalCandies;
+    }
+
+    public static void main(String[] args) {
+
+        CandyDistributionSolver solver =
+                new CandyDistributionSolver();
+
+        int[] ratings = {1, 0, 2};
+
+        System.out.println(
+                solver.candy(ratings)
+        );
+    }
+}
