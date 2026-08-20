84c3a8926b53653d0e75ac66920a6093e4204cd7
ayush-h-pawar <ayushhpawar0505@gmail.com>
2026-08-20 10:30:00 +0530

    Rename class to KthLargestArrayElement1

---
+++ KthLargestArrayElement1.java
@@ -0,0 +1,40 @@
+import java.util.PriorityQueue;
+
+public class KthLargestArrayElement1 {
+
+    public int findKthLargest(
+            int[] numbers,
+            int k) {
+
+        PriorityQueue<Integer> minHeap =
+                new PriorityQueue<>();
+
+        for (int number : numbers) {
+
+            minHeap.offer(number);
+
+            if (minHeap.size() > k) {
+                minHeap.poll();
+            }
+        }
+
+        return minHeap.peek();
+    }
+
+    public static void main(String[] args) {
+
+        KthLargestArrayElement solver =
+                new KthLargestArrayElement();
+
+        int[] numbers = {
+                3, 2, 1, 5, 6, 4
+        };
+
+        System.out.println(
+                solver.findKthLargest(
+                        numbers,
+                        2
+                )
+        );
+    }
+}
