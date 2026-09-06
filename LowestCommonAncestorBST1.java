9b4e0fdaef16f5e19197bcfe285a652c0702a8f1
ayush-h-pawar <ayushhpawar0505@gmail.com>
2026-09-06 21:14:53 +0530

    Rename class to LowestCommonAncestorBST1

---
+++ LowestCommonAncestorBST1.java
@@ -0,0 +1,30 @@
+public class LowestCommonAncestorBST1 {
+
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+    }
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        while (root != null) {
+            if (p.val < root.val && q.val < root.val) {
+                root = root.left;
+            } else if (p.val > root.val && q.val > root.val) {
+                root = root.right;
+            } else {
+                return root;
+            }
+        }
+
+        return null;
+    }
+}
+
+// Time Complexity: O(h)
+// Space Complexity: O(1)
+// LeetCode: 235 - Lowest Common Ancestor of a Binary Search Tree
