e19e33b3e25cd9eaba297bee50c9aa75f8e903a6
ayush-h-pawar <ayushhpawar0505@gmail.com>
2026-09-08 20:52:46 +0530

    Rename MaximumDepthBinaryTree to MaximumDepthBinaryTree1

---
+++ MaximumDepthBinaryTree1.java
@@ -0,0 +1,27 @@
+public class MaximumDepthBinaryTree1 {
+
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+    }
+
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        int leftDepth = maxDepth(root.left);
+        int rightDepth = maxDepth(root.right);
+
+        return 1 + Math.max(leftDepth, rightDepth);
+    }
+}
+
+// Time Complexity: O(n)
+// Space Complexity: O(h)
+// LeetCode: 104 - Maximum Depth of Binary Tree
