331c6f080d1acfef1fe7602d8dc8df904cf3afe9
ayush-h-pawar <ayushhpawar0505@gmail.com>
2026-09-13 14:43:27 +0530

    Create InvertBinaryTree.java

---
+++ InvertBinaryTree.java
@@ -0,0 +1,31 @@
+public class InvertBinaryTree {
+
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+    }
+
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+
+        invertTree(root.left);
+        invertTree(root.right);
+
+        return root;
+    }
+}
+
+// Time Complexity: O(n)
+// Space Complexity: O(h)
+// LeetCode: 226 - Invert Binary Tree
