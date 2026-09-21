639adeb8249f12c1b4499ee89a0613d19a33fe98
ayush-h-pawar <ayushhpawar0505@gmail.com>
2026-09-21 15:39:06 +0530

    Add ContainerWaterMaximizer class with maxArea method
    Implement maxArea method to calculate maximum water container area.

---
+++ ContainerWaterMaximizer.java
@@ -0,0 +1,28 @@
+public class ContainerWaterMaximizer {
+
+    public int maxArea(int[] height) {
+        int left = 0;
+        int right = height.length - 1;
+        int maxArea = 0;
+
+        while (left < right) {
+            int width = right - left;
+            int currentHeight = Math.min(height[left], height[right]);
+
+            int currentArea = width * currentHeight;
+            maxArea = Math.max(maxArea, currentArea);
+
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        return maxArea;
+    }
+}
+
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+// LeetCode: 11 - Container With Most Water
